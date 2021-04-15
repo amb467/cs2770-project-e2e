db50e742ba78220d8f5fd5c630765aec6a233fd5
Amanda <amb467@pitt.edu>
2021-04-15 16:55:58 -0400

    m

--- visualize.py
+++ visualize.py
@@ -40,7 +40,7 @@ def get_images(image_count, config, root_dir):
         else:
             raise Exception(f'visualize.get_images: no such image: {img_file_path}')
         
-        image = torch.stack(transform(image), 0)
+        image = torch.stack([transform(image)], 0)
         images.append(image)
         
     return img_ids, images
