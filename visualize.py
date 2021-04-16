497b7a8d137894a85e243fc012c2a1464523b3d4
Amanda <amb467@pitt.edu>
2021-04-16 14:46:12 -0400

    m

--- visualize.py
+++ visualize.py
@@ -79,7 +79,8 @@ def get_up_to_eight(lst):
 	
 	if l > 8:
 		step = math.floor(float(l) / 8.0)
-		s = slice(0, l-1, step)
+		offset = l % 8
+		s = slice(offset, l-1, step)
 		lst = lst[s]
 	
 	return lst
