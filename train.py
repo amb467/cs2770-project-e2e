ef1b36e39e5e688fc2c6f8ebea62e0389891fd90
Amanda <amb467@pitt.edu>
2021-04-16 15:26:08 -0400

    m

--- train.py
+++ train.py
@@ -32,7 +32,7 @@ optimizer = torch.optim.Adam(params, lr=config['learning_rate'])
 total_step = len(data_loader)
 for epoch in range(1,config['num_epochs']+1):
     for i, (images, categories, questions, lengths) in enumerate(data_loader):
-        categories = [category.to(device) for category in categories]
+        categories = categories.to(device)
         print(f'Categories: {categories}')
         """
         # Set mini-batch dataset
