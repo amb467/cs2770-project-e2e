8e2a293c7accdeb42bcc0b98a95d35361334e1c0
Amanda <amb467@pitt.edu>
2021-04-18 20:33:50 -0400

    m

--- train.py
+++ train.py
@@ -20,21 +20,19 @@ if not os.path.exists(config['model_dir']):
     os.makedirs(config['model_dir'])
 
 # Put models on device
-#encoder = encoder.to(device)
-#decoder = decoder.to(device)
+encoder = encoder.to(device)
+decoder = decoder.to(device)
 
 # Loss and optimizer
-#criterion = nn.CrossEntropyLoss()
-#params = list(decoder.parameters()) + list(encoder.parameters())
-#optimizer = torch.optim.Adam(params, lr=config['learning_rate'])
+criterion = nn.CrossEntropyLoss()
+params = list(decoder.parameters()) + list(encoder.parameters())
+optimizer = torch.optim.Adam(params, lr=config['learning_rate'])
 
 # Train the models
 total_step = len(data_loader)
 for epoch in range(1,config['num_epochs']+1):
     for i, (images, categories, questions, lengths) in enumerate(data_loader):
-        print(f'Images size: {images.size()}')
-        
-        """
+
         for image, category_list, question in zip(images, categories, questions):
             image = image.to(device)
             category_list = category_list.to(device)
@@ -54,7 +52,6 @@ for epoch in range(1,config['num_epochs']+1):
         if i % config['log_step'] == 0:
             print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
                   .format(epoch, config['num_epochs'], i, total_step, loss.item(), np.exp(loss.item())))
-        """ 
         
-    #torch.save(decoder.state_dict(), os.path.join(config['model_dir'], f'decoder-{epoch}.pth'))
-    #torch.save(encoder.state_dict(), os.path.join(config['model_dir'], f'encoder-{epoch}.pth'))
+    torch.save(decoder.state_dict(), os.path.join(config['model_dir'], f'decoder-{epoch}.pth'))
+    torch.save(encoder.state_dict(), os.path.join(config['model_dir'], f'encoder-{epoch}.pth'))
