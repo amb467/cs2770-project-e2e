c34f5cda94ecccf6d96c28080de590178a33dc24
Amanda <amb467@pitt.edu>
2021-04-19 22:49:27 -0400

    m

--- visualize.py
+++ visualize.py
@@ -127,7 +127,7 @@ if __name__ == '__main__':
             image = img_obj.image.to(device)
             category = img_obj.category.to(device)
             #features = encoder(Variable(image), category, 1, img_obj.density)
-            features = encoder(image, category, 1, img_obj.density)
+            features = encoder(Variable(image), Variable(category), Variable(1), Variable(img_obj.density))
             plt.figure(figsize=(20, 20))
             pcount = 1
         
